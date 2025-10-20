29c490f2059d59dc015332792a4432ea8e77467c
LusyHD <dylanb158@gmail.com>
2025-10-20 12:40:18 -0400

    cleaned code | implement verbose tag

--- main.py
+++ main.py
@@ -6,22 +6,43 @@ from google.genai import types
 
 def main():
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
 
-    client = genai.Client(api_key=api_key)
+    verbose = "--verbose" in sys.argv
+    args = []
 
-    #print("Hello from ai-agent!")
-    if len(sys.argv) < 2:
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if not args:
         print("ERROR: No prompt provided")
         sys.exit(1)
-    else:
-        print("Hello from ai-agent!")
-        prompt = sys.argv[1]
-        response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
-        print(response.text)
 
-    print(f"Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print(f"Response tokens:", response.usage_metadata.candidates_token_count)
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+    generate_content(client, messages, verbose)
+
+    
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages)
+    
+    if verbose:
+        print(f"Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print(f"Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)   
 
 
 if __name__ == "__main__":
